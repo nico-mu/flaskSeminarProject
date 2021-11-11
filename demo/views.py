558b09a016534c44901bab204e6516d456072fe9
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2021-11-11 11:28:34 +0100

    [FEATURE] use template inheritance + add user page

--- demo/views.py
+++ demo/views.py
@@ -1,7 +1,21 @@
-from demo import app
 from flask.templating import render_template
+from demo import app
 
 
 @app.route("/")
+@app.route("/home")
 def index():
-    return render_template("index.html")
+    return render_template("index.j2")
+
+@app.route("/about")
+def about():
+    return render_template("about.j2")
+
+@app.route("/users")
+def users():
+    data = [
+        {'id' : 1, 'name' : 'test1'},
+        {'id' : 2, 'name' : 'test2'},
+        {'id' : 3, 'name' : 'test3'},
+    ]
+    return render_template('users.html.j2', users=data, userName="TestName")
