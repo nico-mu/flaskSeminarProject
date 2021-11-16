b2858a9d3d44816b36efb48899a342dee222d3a6
Nico Muszalczyk <74101214+nico-mu@users.noreply.github.com>
2021-11-16 18:12:00 +0100

    [FEATURE] adding register functionality

--- demo/views.py
+++ demo/views.py
@@ -4,8 +4,8 @@ Contains the views for the application.
 '''
 import os
 from flask.templating import render_template
-from flask import send_from_directory
-from demo import app
+from flask import send_from_directory, redirect, url_for
+from demo import app, db
 from demo.models import *
 from demo.forms import RegisterForm, LoginForm
 
@@ -44,18 +44,25 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-@app.route('/register')
+# Cross Site Request Forgery (CSRF) protection needed in register.html.j2 and login.html.j2 by adding {{ form.hidden_tag() }}
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     '''
     This function renders the register.html.j2 template
     '''
     form = RegisterForm()
+    if form.validate_on_submit():
+        user = User(name=form.username.data, password_hash=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template('register.html.j2', form=form)
 
-@app.route('/login')
+# Cross Site Request Forgery (CSRF) protection needed in register.html.j2 and login.html.j2 by adding {{ form.hidden_tag() }}
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     '''
-    This function renders the register.html.j2 template
+    This function renders the login.html.j2 template
     '''
     form = LoginForm()
     return render_template('login.html.j2', form=form)
